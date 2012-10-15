ea07bee1369abc2157e1dc967be009d742a9cda1
Nicholas Thompson <njt1982@gmail.com>
2012-10-15 16:50:44 +0000

    Fixing incorrect use of hook_library. Adding a clear and float to fix the linked image problem in #1140380

--- js/cloud_zoom.js
+++ js/cloud_zoom.js
@@ -3,6 +3,7 @@ Drupal.behaviors.mybehavior = {
   attach: function (context, settings) {
     // The cloud zoom options are in the HTML.
     $('.cloud-zoom, .cloud-zoom-gallery').CloudZoom();
+    $('.cloud-zoom, .cloud-zoom-gallery').parent().css('float', 'left');
   }
 };
 })(jQuery);
